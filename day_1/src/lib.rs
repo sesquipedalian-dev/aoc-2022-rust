be7387261104dab9a948ac7db24be99e211a4084
sesquipedalian.dev <sesquipedalian.dev@gmail.com>
2022-12-08 14:58:33 -0700

    day 1 part 2

--- day_1/src/lib.rs
+++ day_1/src/lib.rs
@@ -22,7 +22,37 @@ pub fn first(input: &[String]) -> u32 {
 
 
 pub fn second(input: &[String]) -> u32 {
-    return 0
+    let input = input_to_nums(input);
+
+    let (_, max_cals) = input.iter().fold(
+        (0, (vec!(0, 0, 0))),
+        |(cur_cals, max_cals), item|  {
+            match item {
+                // new elf
+                0 => {
+                    let mut new_max: Vec<u32> = vec!();
+                    let mut i = 0;
+                    let mut fill_mode = false;
+                    while new_max.len() < 3 && i < 3 {
+                        // if this is the spot to insert cur_cals
+                        if !fill_mode && (max_cals[i] < cur_cals) {
+                            new_max.push(cur_cals);
+                            fill_mode = true;
+                        } else {
+                            // otherwise grab an entry from the current max_cals
+                            new_max.push(max_cals[i]);
+                            i = i+1;
+                        }
+                    }
+                    (0, new_max)
+                },
+                // accumulate weight in current elf
+                _ => (cur_cals + item, max_cals)
+            }
+        }
+    );
+
+    return max_cals.iter().fold(0, |cur, next| cur + next);
 }
 
 #[cfg(test)]
@@ -44,7 +74,8 @@ mod tests {
             "8000",
             "9000",
             "",
-            "10000"
+            "10000",
+            ""
         );
         input.iter()
             .map(|s: &&str| { String::from(*s) })
@@ -61,6 +92,6 @@ mod tests {
     fn second_test() { 
         let input = example();
         let result = second(&input);
-        assert_eq!(result, 0);
+        assert_eq!(result, 45000);
     }
 }
