fde606a132a33ed9f13fee78f7324fd6985063f4
sesquipedalian.dev <sesquipedalian.dev@gmail.com>
2022-12-09 15:46:55 -0700

    day 4 part 2

--- day_4/src/lib.rs
+++ day_4/src/lib.rs
@@ -1,24 +1,34 @@
 pub fn first(input: &[String]) -> usize {
     let range_pairs = parse_input(input);
     range_pairs
-        .fold(0, |accum, next| {
-            if (next.0.start() <= next.1.start()) && (next.0.end() >= next.1.end()) ||
-                (next.1.start() <= next.0.start()) && (next.1.end() >= next.0.end()){
-                accum + 1
-            } else {
-                accum
-            }
+        .filter(|next| {
+            ((next.0.start() <= next.1.start()) && (next.0.end() >= next.1.end()))
+                || ((next.1.start() <= next.0.start()) && (next.1.end() >= next.0.end()))
         })
+        .count()
 }
 
 pub fn second(input: &[String]) -> usize {
-    0
+    let range_pairs = parse_input(input);
+    range_pairs
+        .filter(|next| {
+            ((next.0.start() <= next.1.start()) && (next.0.end() >= next.1.start()))
+                || ((next.0.start() <= next.1.end()) && (next.0.end() >= next.1.start()))
+        })
+        .count()
 }
 
-pub fn parse_input(input: &[String]) -> impl Iterator<Item=(std::ops::RangeInclusive<usize>, std::ops::RangeInclusive<usize>)> +'_ {
+pub fn parse_input(
+    input: &[String],
+) -> impl Iterator<
+    Item = (
+        std::ops::RangeInclusive<usize>,
+        std::ops::RangeInclusive<usize>,
+    ),
+> + '_ {
     input.iter().filter_map(|line| {
         if line.len() < 1 {
-            return None
+            return None;
         }
 
         let parts = line.split(',');
@@ -26,9 +36,9 @@ pub fn parse_input(input: &[String]) -> impl Iterator<Item=(std::ops::RangeInclu
             let mut parts = part.split('-');
             let start: usize = parts.next().unwrap().parse().unwrap();
             let end: usize = parts.next().unwrap().parse().unwrap();
-            start ..= end
+            start..=end
         });
-        
+
         Some((ranges.next().unwrap(), ranges.next().unwrap()))
     })
 }
@@ -64,6 +74,6 @@ mod tests {
     fn second_test() {
         let input = example();
         let result = second(&input);
-        assert_eq!(result, 0);
+        assert_eq!(result, 4);
     }
 }
