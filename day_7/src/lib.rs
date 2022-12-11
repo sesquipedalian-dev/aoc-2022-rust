560b834edacc1342f1c85c4813d7dba35a9e758e
sesquipedalian.dev <sesquipedalian.dev@gmail.com>
2022-12-10 17:58:20 -0700

    day 7 part 2

--- day_7/src/lib.rs
+++ day_7/src/lib.rs
@@ -1,7 +1,39 @@
 use std::collections::HashMap;
 
 pub fn first(input: &[String]) -> usize {
-    let mut dir_sizes: HashMap<String, usize> = HashMap::new();
+    let dir_sizes = compute_dir_sizes(input);
+    println!("dir sizes {:?}", dir_sizes);
+
+    let mut sum_of_smaller_dirs: usize = 0;
+    for (_, size) in dir_sizes.iter() {
+        if *size <= 100_000 {
+            sum_of_smaller_dirs += size;
+        }
+    }
+    sum_of_smaller_dirs
+}
+
+pub fn second(input: &[String]) -> usize {
+    let dir_sizes = compute_dir_sizes(input);
+    println!("dir sizes {:?}", dir_sizes);
+
+    let current_used_space = dir_sizes.get(&String::from("/")).unwrap();
+    let current_unused_space = 70_000_000 - current_used_space;
+    let needed_additional_space = 30_000_000 - current_unused_space;
+
+    dir_sizes
+        .iter()
+        .fold(30_000_000, |min, (_, next_dir_size)| {
+            if *next_dir_size > needed_additional_space && *next_dir_size < min {
+                *next_dir_size
+            } else {
+                min
+            }
+        })
+}
+
+fn compute_dir_sizes(input: &[String]) -> HashMap<String, usize> {
+    let mut dir_sizes = HashMap::new();
     let mut dir_names: Vec<&str> = vec![];
     for line in input {
         if line.starts_with("$") {
@@ -22,7 +54,7 @@ pub fn first(input: &[String]) -> usize {
             let file_size: usize = line.split_whitespace().next().unwrap().parse().unwrap();
             let mut dir_name_accum = String::from("");
             for dir_name in dir_names.iter() {
-                dir_name_accum.push_str(dir_name);
+                dir_name_accum.push_str(dir_name); // make the hash key the path to here to avoid duplicate name problems
                 let existing_size = dir_sizes
                     .get(&dir_name_accum)
                     .map(|r| *r)
@@ -32,19 +64,7 @@ pub fn first(input: &[String]) -> usize {
         }
     }
 
-    println!("dir sizes {:?}", dir_sizes);
-
-    let mut sum_of_smaller_dirs: usize = 0;
-    for (_, size) in dir_sizes.iter() {
-        if *size <= 100_000 {
-            sum_of_smaller_dirs += size;
-        }
-    }
-    sum_of_smaller_dirs
-}
-
-pub fn second(input: &[String]) -> usize {
-    0
+    dir_sizes
 }
 
 #[cfg(test)]
@@ -75,7 +95,7 @@ mod tests {
             "4060174 j",
             "8033020 d.log",
             "5626152 d.ext",
-            "7214297 k",
+            "7214296 k",
         ];
         input.iter().map(|s: &&str| String::from(*s)).collect()
     }
@@ -108,6 +128,6 @@ mod tests {
     fn second_test() {
         let input = example();
         let result = second(&input);
-        assert_eq!(result, 0);
+        assert_eq!(result, 24933642);
     }
 }
