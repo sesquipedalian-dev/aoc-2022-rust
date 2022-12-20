2747ed0907c477540d9b982b3eb86bd7004d7c18
sesquipedalian.dev <sesquipedalian.dev@gmail.com>
2022-12-20 11:29:14 -0700

    day 20 part 2 in progress?

--- day_20/src/lib.rs
+++ day_20/src/lib.rs
@@ -1,10 +1,19 @@
 use common::input_to_nums;
 
-pub fn first(input: &[String]) -> i32 {
-    let input: Vec<i32> =     input
+pub fn first(input: &[String]) -> i64 {
+    mix_it_up(&input, 1, 1)
+}
+
+pub fn second(input: &[String]) -> i64 {
+    mix_it_up(&input, 811589153i64, 10)
+}
+
+fn mix_it_up(input: &[String], decryption_key: i64, max_mixes: usize) -> i64 {
+    let input: Vec<i64> = input
         .iter()
-        .map(|s| s.parse::<i32>().ok())
+        .map(|s| s.parse::<i64>().ok())
         .flatten()
+        .map(|v| v * decryption_key)
         .collect();
 
     // vector holds list nodes; the nodes prev and next are indices in this list
@@ -16,7 +25,7 @@ pub fn first(input: &[String]) -> i32 {
         } else { 
             i - 1
         };
-        circular_list_nodes.push(ListNode::new(input[i], i + 1, prev_i));
+        circular_list_nodes.push(ListNode::new(*item, i + 1, prev_i));
     }
     // make it circular
     circular_list_nodes[input.len() - 1].next = 0;
@@ -24,72 +33,67 @@ pub fn first(input: &[String]) -> i32 {
     // keep track of the 'first' item in the list (so we can fid the 1000th etc)
     let mut circular_list_head_i = 0usize;
 
-    // mix all the entries
-    for i in 0..input.len() {
-        
-        let current = circular_list_nodes[i];
+    for max_i in 0 .. max_mixes {
+        // mix all the entries
+        for i in 0..input.len() {
+            let current = circular_list_nodes[i];
+            let current_val = current.val;
 
-        // // print
-        // let mut current_i = circular_list_head_i;
-        // for _ in 0..input.len() {
-        //     print!("{}, ", circular_list_nodes[current_i].val);
-        //     current_i = circular_list_nodes[current_i].next;
-        // }
-        // println!();
-        // println!("{:?} {}", circular_list_nodes, circular_list_head_i);
+            if current_val == 0 {
+                continue;
+            }
 
-        if current.val == 0 {
-            continue;
-        }
-
-        let moving_right = current.val < 0;
-        for _ in 0..(current.val.abs() as usize) { // move as many times as the value specified
-            // moving left is the same as moving our previous right
-            let current_i = if moving_right {
-                circular_list_nodes[i].prev
-            } else { 
-                i
+            let moves = if current_val < 0 { // moving left is the same as moving right an opposite amount
+                circular_list_nodes.len() - 1 - ((current.val.abs() as usize) % circular_list_nodes.len())
+            } else {
+                (current_val as usize) % circular_list_nodes.len()
             };
 
-            // move right
-            let prev_i = circular_list_nodes[current_i].prev;
+            // example: move 2 3 to the right 
+            // p c n   np nn  
+            // 1 2 3 4 5  6
+            // p n   np c nn
+            // 1 3 4 5  2 6
             
-            let next_i = circular_list_nodes[current_i].next;
+            let prev_i = circular_list_nodes[i].prev;
+            let next_i = circular_list_nodes[i].next;
+            
+            let mut new_prev_i  = i;
+            for _ in 0..moves {
+                new_prev_i = circular_list_nodes[new_prev_i].next;
+            }
 
-            let next_next_i = circular_list_nodes[next_i].next;
+            let new_next_i = circular_list_nodes[new_prev_i].next;
 
             circular_list_nodes[prev_i].next = next_i;
 
             circular_list_nodes[next_i].prev = prev_i;
-            circular_list_nodes[next_i].next = current_i;
 
-            circular_list_nodes[current_i].prev = next_i;
-            circular_list_nodes[current_i].next = next_next_i;
+            circular_list_nodes[new_prev_i].next = i;
 
-            circular_list_nodes[next_next_i].prev = current_i;
-  
+            circular_list_nodes[i].prev = new_prev_i;
+            circular_list_nodes[i].next = new_next_i;
+
+            circular_list_nodes[new_next_i].prev = i;
         }
-    }
 
-            // // print
-            // let mut current_i = circular_list_head_i;
-            // for _ in 0..input.len() {
-            //     print!("{}, ", circular_list_nodes[current_i].val);
-            //     current_i = circular_list_nodes[current_i].next;
-            // }
-            // println!();
+        // print
+        let mut current_i = circular_list_head_i;
+        for _ in 0..input.len() {
+            print!("{}, ", circular_list_nodes[current_i].val);
+            current_i = circular_list_nodes[current_i].next;
+        }
+        println!();
+    }
 
     // get the items in question
     // find index of 0
-    // let mut i = 0;
     while circular_list_nodes[circular_list_head_i].val != 0 {
         circular_list_head_i = circular_list_nodes[circular_list_head_i].next;
     }
-    // println!("location of 0: {}", i);
 
     let mut sum = 0;
     for i in 0..=3000 {
-        
         let current_item = circular_list_nodes[circular_list_head_i];
         if i == 1000 || i == 2000 || i == 3000 {
             println!("nth item? {} {}", i , current_item.val);
@@ -99,20 +103,15 @@ pub fn first(input: &[String]) -> i32 {
     }
     sum
 }
-
-pub fn second(input: &[String]) -> usize {
-    0
-}
-
 #[derive(Debug, Eq, PartialEq, Hash, Copy, Clone)]
 struct ListNode {
-    val: i32,
+    val: i64,
     next: usize,
     prev: usize,
 }
 
 impl ListNode {
-    fn new(val: i32, next: usize, prev: usize) -> ListNode { 
+    fn new(val: i64, next: usize, prev: usize) -> ListNode { 
         ListNode{val, next, prev}
     }
 }
@@ -145,6 +144,6 @@ mod tests {
     fn second_test() {
         let input = example();
         let result = second(&input);
-        assert_eq!(result, 0);
+        assert_eq!(result, 1623178306);
     }
 }
